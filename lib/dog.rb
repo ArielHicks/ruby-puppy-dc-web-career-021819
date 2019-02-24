d846dc13c2433dfa54ba15e7799ae89c34cd4a64
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-24 18:25:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,4 @@
 class Dog
 
-  # attr_accessor :
-
-  @@all = []
-  # 
-  # def initialize(all)
-  #   @@all << self
-  # end
-  #
-  # def all
-  #   self.each do |d|
-  #   if self << @@all
-  # end
-  #
-  # def clear_all
-  #   all.clear
-  # end
 
 end
